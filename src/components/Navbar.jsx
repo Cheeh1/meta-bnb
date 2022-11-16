617d4c48c949e0fa6bde53b2aba1c560d42a3d94
Divine Edwin <edwindivine5@gmail.com>
2022-11-16 10:57:38 +0100

    mobile navbar
    craeted mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom'
 import { logo, menuBtn} from '../assets';
 import NavbarCss from '../styles/Navbar.module.css'
@@ -19,7 +20,7 @@ const Navbar = () => {
                         <Link className={NavbarCss.navlink} to='/placeToStay'>Place to stay</Link>
                     </li>
                     <li>
-                        <a className={NavbarCss.navlink} href='#nfts'>NFTS</a>
+                        <a className={NavbarCss.navlink} href='#nfts'>NFTs</a>
                     </li>
                     <li>
                         <a className={NavbarCss.navlink} href='#community'>Community</a>
@@ -34,6 +35,26 @@ const Navbar = () => {
             <nav className={NavbarCss.mobileNav}>
                  <img src={ logo } alt="logo" />
                  <img className={NavbarCss.mobileBtn} src={menuBtn} alt="logo" />
+            </nav>
+
+            <nav className={NavbarCss.mobileFlex}>
+                <ul className={NavbarCss.mobileList}>
+                    <li>
+                    <Link className={NavbarCss.mobilelink} to='/'>Home</Link>
+                    </li>
+                    <li>
+                    <Link className={NavbarCss.mobilelink} to='/placeToStay'>Place to stay</Link>
+                    </li>
+                    <li>
+                        <a className={NavbarCss.mobilelink} href='nfts'>NFTs</a>
+                    </li>
+                    <li>
+                        <a className={NavbarCss.mobilelink} href='#community'>Community</a>
+                    </li>
+                </ul>
+                <div>
+                    <button className={NavbarCss.mobileBtn2}>Connect wallet</button>
+                </div>
             </nav>
         </>
     )
