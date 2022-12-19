57e206d8ea3f64ee97e6e64e714cc6dd66949ebe
Divine Edwin <edwindivine5@gmail.com>
2022-12-20 00:31:37 +0100

    changed a component look
    changed the dropdown of the options components to carousel, so it can be easily be responsive.

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -5,7 +5,6 @@ import OptionsCss from "../styles/Options.module.css";
 const Options = () => {
   return (
     <>
-      {/* Desktop view */}
       <section className={OptionsCss.flex}>
         <ul className={OptionsCss.list}>
           <li className={OptionsCss.link}>Resturant</li>
@@ -17,33 +16,6 @@ const Options = () => {
           <li className={OptionsCss.link}>Off-grid</li>
           <li className={OptionsCss.link}>Farm</li>
         </ul>
-        <div className={OptionsCss.btn}>
-          <p className={OptionsCss.btnTxt}>Location</p>
-          <img src={setting} alt="logo" />
-        </div>
-      </section>
-
-      {/* Mobile view */}
-      <section className={OptionsCss.navFlex}>
-        <div className={OptionsCss.navDropdown}>
-          <div className={OptionsCss.dropdown}>
-            <div className={OptionsCss.dropdownBtn}>
-              <p className={OptionsCss.btnTxt}>Location Options</p>
-              <img src={arrow} alt="logo" />
-            </div>
-            <div className={OptionsCss.dropdownContent}>
-              <p>Resturant</p>
-              <p>Cottage</p>
-              <p>Castle</p>
-              <p>fantast city</p>
-              <p>beach</p>
-              <p>Carbins</p>
-              <p>Off-grid</p>
-              <p>Farm</p>
-            </div>
-          </div>
-        </div>
-
         <div className={OptionsCss.btn}>
           <p className={OptionsCss.btnTxt}>Location</p>
           <img src={setting} alt="logo" />
